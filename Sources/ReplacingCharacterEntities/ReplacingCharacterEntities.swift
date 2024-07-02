5652b350f157c67b40b65c5c0fbf2fb83e01c5dd
stefanspringer1 <stefan.springer@icloud.com>
2024-07-02 17:12:55 +0200

    README

--- Sources/ReplacingCharacterEntities/ReplacingCharacterEntities.swift
+++ Sources/ReplacingCharacterEntities/ReplacingCharacterEntities.swift
@@ -1,15 +1,12 @@
 import Foundation
 
-/// A macro to replace the character classes from [SwiftUtilities](https://github.com/stefanspringer1/SwiftUtilities) in the form `${MY_CHARACTER_CLASS}` in a regular expression text, returning a literal regular expression. (The dollar sign can be escaped using a backslash.) You will get a compiler error if you have chosen a non-existing character class.
-///
-/// To use this macro, you need to know how many groups are in the regular expression and choose the according macro name. If there are `<n>` groups, you will have to call the macro by `#ReplacingCharacterEntitiesAnd<n>Groups(...)`, e.g `#ReplacingCharacterEntitiesAnd2(...)` if there are two groups (exception: when having one group, you need to call `ReplacingCharacterEntitiesAnd1Group`). Currently, you can call the macro for up to seven groups. You will get a compiler error if you have chosen the wrong macro name.
+/// A macro to replace the usual character entities (cf. [SwiftUtilities](https://github.com/stefanspringer1/SwiftUtilities)) in the (XML notation) form `&MY_CHARACTER_ENTITY;`, returning the according text. (The ampersand can be escaped using a backslash.) You will get a compiler error if you have chosen a non-existing entity name.
 ///
 /// Usage (note that the type annotation is not necessary):
 ///
-/// ```swift
-/// // note that the type annotation is not necessary:
-/// let regex: Regex<Substring> = #ReplacingCharacterEntitiesAnd1Group("[${LATIN_LETTERS}]")
-/// print("123 hello!".replacing(regex, with: "x")) // "123 xxxxx!"
+/// ```swift// note that the type annotation is not necessary:
+/// let result: String = #ReplacingCharacterEntities("${auml}${alpha}")
+/// print(result) // "äα"
 /// ```
 @freestanding(expression)
 public macro replacingCharacterEntities(_ text: String) -> String = #externalMacro(module: "ReplacingCharacterEntitiesMacros", type: "ReplacingCharacterEntities")
