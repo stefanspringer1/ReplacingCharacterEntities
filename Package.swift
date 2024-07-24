6528a9a3a0c4cb5113a388703e9d351f75d8c707
stefanspringer1 <stefan.springer@icloud.com>
2024-07-24 12:39:41 +0200

    using SwiftUtilities 0.1.37

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
-        .package(url: "https://github.com/stefanspringer1/SwiftUtilities.git", from: "0.1.36"),
+        .package(url: "https://github.com/stefanspringer1/SwiftUtilities.git", from: "0.1.37"),
     ],
     targets: [
         .macro(
